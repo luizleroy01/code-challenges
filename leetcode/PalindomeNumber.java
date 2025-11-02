ac61b6e9a6126459271a4fbe6d0d31cb4ae35e07
Luiz Leroy <leroy291001@gmail.com>
2025-11-02 00:44:44 -0300

    feat: complete palindome exercise

--- leetcode/PalindomeNumber.java
+++ leetcode/PalindomeNumber.java
@@ -1,15 +1,35 @@
+
+import java.util.ArrayList;
+
 public class PalindomeNumber {
     public static void main(String[] args) {
-        boolean result = isPalindrome(10);
+        boolean result = isPalindrome(121);
+
+        System.out.println( result ? "eh Palindomo" : "Não eh palindromo");
     }
 
-    public boolean isPalindrome(int x){
+    public static boolean isPalindrome(int x){
         if(x < 0) return false;
+        if(x < 10) return true;
 
         int div = x/10;
         int mod = x % 10;
 
+        ArrayList<String> v = new ArrayList<>();
+        v.add(""+mod);
 
-        return true;
+        while(div > 9){
+            x = div;
+            div = x/10;
+            mod = x % 10;
+            v.add(""+mod);
+        }
+
+        v.add(""+div);
+
+        var reverseList = v.reversed();
+
+       return v.equals(reverseList);
+
     }
 }
